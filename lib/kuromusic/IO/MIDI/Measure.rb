13a2797657a8d256dcbb1bded9b19a0a07cec5d2
黒澤 預生 <yonamachan3@gmail.com>
2016-06-25 04:32:58 +0900

    BPM変更に対応

--- lib/kuromusic/IO/MIDI/Measure.rb
+++ lib/kuromusic/IO/MIDI/Measure.rb
@@ -1,10 +1,14 @@
 require_relative "Chord"
+require_relative "Event"
 
 class Midi
   private
     module Measure
       def to_bin()
-        @chords.map {|c|
+        @metas.map {|m|
+          m.extend(Midi::Event)
+          m.to_bin
+        }.flatten.concat @chords.map {|c|
           c.extend(Midi::Chord)
           c.to_bin
         }.flatten!
